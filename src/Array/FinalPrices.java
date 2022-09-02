457db8923f8b8ab7884ff585c0209562cbf7eb73
rundongzhichu <2298812879@qq.com>
2022-09-02 22:17:20 +0800

    Algorithm practice

---
+++ src/Array/FinalPrices.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class FinalPrices {
+    public int[] finalPrices(int[] prices) {
+        for(int i=0;i<prices.length-1;i++){
+            for(int j=i+1; j<prices.length; j++){
+                if(prices[j]<=prices[i]){
+                    prices[i] -= prices[j];
+                }
+            }
+        }
+        return prices;
+    }
+}
