9ef1d752f7c3c1bbd36ebf9c04204eee72fa03b2
rundongzhichu <2298812879@qq.com>
2024-03-13 22:55:45 +0800

    add spi demo

---
+++ SPIDemo/src/main/java/com/spi/demo/spidemo/SPITest.java
@@ -0,0 +1,10 @@
+package com.spi.demo.spidemo;
+
+public class SPITest {
+
+    public static void main(String[] args) {
+        LoggerService loggerService = LoggerService.getService();
+        loggerService.info("你好");
+        loggerService.debug("测试Java SPI 机制");
+    }
+}
