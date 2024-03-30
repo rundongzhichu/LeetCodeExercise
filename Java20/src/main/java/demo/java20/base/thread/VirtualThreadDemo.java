b3578fa503e9e456c9250e2fdc607d749733c321
rundongzhichu <2298812879@qq.com>
2024-03-30 18:31:02 +0800

    add VirtualThreadDemo learning

---
+++ Java20/src/main/java/demo/java20/base/thread/VirtualThreadDemo.java
@@ -0,0 +1,37 @@
+package demo.java20.base.thread;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class VirtualThreadDemo {
+
+    /**
+     * E:\IdeaProjects\LeetCodeExercise\Java20\src\main\java>java --enable-preview demo.java20.base.thread.VirtualThreadDemo
+     * java --enable-preview --source 20 VirtualThreadDemo.java -Xlint:preview
+     *
+     * idea 直接build运行的话会出现（这个可能是idea本身的问题，java命令行能执行）：
+     java: 源发行版 16 与 --enable-preview 一起使用时无效
+     （仅发行版 20 支持预览语言功能）
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+            ReentrantLock lock = new ReentrantLock();
+            Thread.startVirtualThread(() -> {
+                lock.lock();     // <------ 这里确保锁已经被另一个虚拟线程持有
+            });
+            Thread.sleep(1000);
+            Thread.startVirtualThread(() -> {
+                System.out.println("first");
+                lock.lock();
+                try {
+                    System.out.println("second");
+                } finally {
+                    lock.unlock();
+                }
+                System.out.println("third");
+            });
+            Thread.sleep(Long.MAX_VALUE);
+        }
+
+
+}
