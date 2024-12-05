8439a40bd88169b7dabc162ad55c605b5fdb0717
sunshoucai <17396248231@163.com>
2024-12-05 19:01:45 +0800

    kafka rocketMq对比

--- src/AlgotithmExercise/Main.java
+++ src/AlgotithmExercise/Main.java
@@ -2,60 +2,28 @@ package AlgotithmExercise;
 
 
 import AlgotithmExercise.BinaryTree.TreeNode;
+import AlgotithmExercise.DoublePointer.ListNode;
 
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.concurrent.Semaphore;
+import java.util.stream.Stream;
 
 public class Main {
+
+    private Semaphore aSem = new Semaphore(1);
+    private Semaphore bSem = new Semaphore(0);
+    private Semaphore cSem = new Semaphore(0);
 
     public static void main(String[] args) {
         new Main().test();
     }
 
     public void test() {
-        int[] nums = new int[] {1,2,3,4,3,2,1};
-        int[] idxs = twoSum(nums, 4);
-        System.out.println("idxs[0] = " + idxs[0] + " , idxs[1] = " + idxs[1]);
-
-        idxs = twoSum1(nums, 4);
-        System.out.println("idxs[0] = " + idxs[0] + " , idxs[1] = " + idxs[1]);
+        System.out.println("aSem = " + aSem);
     }
 
-    public int[] twoSum(int[] nums, int target) {
-        int len = nums.length;
-        for (int i=0;i < len - 1; i++) {
-            for (int j = i + 1; j < len; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        return null;
-    }
 
-    public int[] twoSum1(int[] nums, int target) {
-        int len = nums.length;
-        Map<Integer, List<Integer>> statistics = new HashMap<>();
-
-        for (int i = 0; i < len; i++) {
-            List<Integer> lis = statistics.getOrDefault(nums[i], new ArrayList<>());
-            lis.add(i);
-            statistics.put(nums[i], lis);
-        }
-
-        for (int i=0;i < len; i++) {
-            int remain = target - nums[i];
-            if(statistics.containsKey(remain)) {
-                for (Integer idx :
-                        statistics.get(remain)) {
-                    if(idx != i) {
-                        return new int[]{i, idx};
-                    }
-                }
-            }
-        }
-        return null;
-    }
 
 }
 
